e4b87d2e1989367b600fc356f77db74183487c07
Rahul Yadav <rahulyadav@Rahuls-MacBook-Pro.local>
2025-01-18 12:47:19 +0530

    changes are done

--- app.py
+++ app.py
@@ -1,45 +1,48 @@
-import streamlit as st
+import streamlit as st 
 from phi.agent import Agent
 from phi.model.google import Gemini
 from phi.tools.duckduckgo import DuckDuckGo
-from google.generativeai import upload_file, get_file
-from google.generativeai import genai
+from google.generativeai import upload_file,get_file
+import google.generativeai as genai
 
 import time
-from pathlib import path
+from pathlib import Path
+
 import tempfile
+
 from dotenv import load_dotenv
 load_dotenv()
+
 import os
 
 API_KEY=os.getenv("GOOGLE_API_KEY")
 if API_KEY:
     genai.configure(api_key=API_KEY)
-    
-#page configuration
-st.set_page_config(
-    page_title="Multimodal AI Agent-Video Summarizer",
-    page_icon="🤖",
-    layout="wide",
-    
-) 
 
-st.tittle("phi - Multimodal AI summerizer Agent")
-st.header("powered by Gemini 2.0 Flash Exp")
+# Page configuration
+st.set_page_config(
+    page_title="Multimodal AI Agent- Video Summarizer",
+    page_icon="🎥",
+    layout="wide"
+)
+
+st.title("Phidata Video AI Summarizer Agent 🎥🎤🖬")
+st.header("Powered by Gemini 2.0 Flash Exp")
+
+
 @st.cache_resource
 def initialize_agent():
     return Agent(
-        name="Video AI Summerizer",
-        model=Gemini(id="gemini-2-0-flash-exp"),
+        name="Video AI Summarizer",
+        model=Gemini(id="gemini-2.0-flash-exp"),
         tools=[DuckDuckGo()],
         markdown=True,
-    )  
-    
-    
-## initialize the agent
-multimodal_agent = initialize_agent()
+    )
 
-#file uploader
+## Initialize the agent
+multimodal_Agent=initialize_agent()
+
+# File uploader
 video_file = st.file_uploader(
     "Upload a video file", type=['mp4', 'mov', 'avi'], help="Upload a video for AI analysis"
 )
@@ -81,7 +84,7 @@ if video_file:
                     )
 
                     # AI agent processing
-                    response =multimodal_agent.run(analysis_prompt, videos=[processed_video])
+                    response = multimodal_Agent.run(analysis_prompt, videos=[processed_video])
 
                 # Display the result
                 st.subheader("Analysis Result")
@@ -107,4 +110,3 @@ st.markdown(
     unsafe_allow_html=True
 )
 
- 
