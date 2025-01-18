c602592423c8f281973528b98706691e8bb81243
Rahul Yadav <rahulyadav@Rahuls-MacBook-Pro.local>
2025-01-18 12:24:54 +0530

    files added

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from phi.agent import Agent
 from phi.model.google import Gemini
 from phi.tools.duckduckgo import DuckDuckGo
-from google.generativeai import genai
 from google.generativeai import upload_file, get_file
+from google.generativeai import genai
 
 import time
 from pathlib import path
